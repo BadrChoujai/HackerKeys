74810d1e7cfda40afe94b92dd1d2ad18d60470b3
BadrChoujai <57566300+Badr504@users.noreply.github.com>
2020-06-29 12:25:20 +0100

    Between two sets problem #13 solved in Javascript.

---
+++ Between Two Sets#13/Between Two Sets#13 (Solution).js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param {array} a
+ * @param {array} b
+ */
+function getTotalX(a, b) {
+  let validCount = 0;
+
+  for (let x = 1; x <= 100; x++) {
+    if (a.every((int) => x % int == 0)) {
+      if (b.every((int) => int % x == 0)) {
+        validCount++;
+      }
+    }
+  }
+
+  return validCount;
+}
